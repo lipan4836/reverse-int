ccd8b359362a450afebd5f1e839d986b3bee442f
Anton <lipan48@gmail.com>
2023-08-17 20:21:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let result
+  let nString = n.toString()
+  if (nString[0] !== '-') {
+    result = Array.from(nString).reverse().join('')
+  } else {
+    result = Array.from(nString).slice(1).reverse().join('')
+  }
+  return result
 }
